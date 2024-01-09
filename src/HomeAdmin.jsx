dcb772d5a9e9a6ca6cf8ea22a9bbff3385f24dfd
SenPatthamwan <sen.patthamwan@gmail.com>
2024-01-09 22:53:51 +0700

    Modified css: text Update button

--- src/HomeAdmin.jsx
+++ src/HomeAdmin.jsx
@@ -108,7 +108,7 @@ const HomeAdmin = ({employees, getData}) => {
                             <div>
                                 {employee.action}
                                     <button
-                                        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        className="bg-pink-500 text-white active:bg-pink-600 font-bold text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                         type="button"
                                         onClick={() => showUpdateModal(employee.id)}
                                     >
